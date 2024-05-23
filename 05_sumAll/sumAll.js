eeb65be024ecef11464ff9fbb7ca14548fbf6914
hirian <vicjaime922@gmail.com>
2024-05-23 14:44:35 -0600

    Solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(arg1,arg2) {
 
+    if (typeof arg1 !='number' || typeof arg2!='number') return 'ERROR';
+
+    let maxN = Math.max(arg1,arg2);
+    let minN = Math.min(arg1,arg2);
+
+    if (minN<=0) return 'ERROR';
+
+    let ans=0;
+    for (let i = minN; i <= maxN; i++) {
+        ans+=i;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
