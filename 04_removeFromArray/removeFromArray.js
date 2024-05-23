daf1162f1ec3679efadbd9b97cfa55b65e110d79
hirian <vicjaime922@gmail.com>
2024-05-23 14:31:07 -0600

    Solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (arg of args){
+        arr = arr.filter(e => e !== arg);
+    }
+    return arr;
 };
 
 // Do not edit below this line
