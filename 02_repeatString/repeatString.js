9d4db9e9072d0f0d73539696c71cf7d1492727cd
hirian <vicjaime922@gmail.com>
2024-05-23 06:08:57 -0600

    Solutions to exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(input,times) {
+    if (times<0) return 'ERROR';
 
+    let ans = "";
+    for (let i = 0; i < times; i++) {
+        ans+=input;        
+    }
+    return ans;
 };
 
 // Do not edit below this line
